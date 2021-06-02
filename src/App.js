e99afba0bfdb5b4496caabbb2b4e7d3f1b51f03e
Isabbelle <isabelle.muresan@gmail.com>
2021-06-02 13:49:34 +0200

    fetching restaurant data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState, Fragment } from 'react';
 
 function App() {
-  const [name, setName] = useState('khouloud');
-  return <div className="App">Hello world!</div>;
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    const getRestaurants = async () => {
+      const response = await fetch(
+        `https://redi-final-restaurants.herokuapp.com/restaurants`
+      );
+      const data = await response.json();
+      console.log(data.results);
+      setRestaurants(data.results);
+    };
+    getRestaurants();
+  }, []);
+
+  return (
+    <>
+      <ul>
+        {restaurants.map((item) => {
+          return (
+            <li key={item.id}>
+              {item.name}
+              {item.formatted_address}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="App">Hello world!</div>
+    </>
+  );
 }
 
 export default App;
