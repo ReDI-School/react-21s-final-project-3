4282fe27104661bf0bdabd83882790bc3cad0f66
khoolood <khoolood.loolood@gmail.com>
2021-06-04 12:28:18 +0200

    Adding_Components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 import './App.css';
-import { useEffect, useState, Fragment } from 'react';
+import { NavBar } from './Components/NavBar';
+import { SearchBar } from './Components/SearchBar';
+import { ListOfRestaurants } from './Components/ListOfRestaurants';
+import { Footer } from './Components/Footer';
 
 function App() {
-  const [restaurants, setRestaurants] = useState([]);
-
-  useEffect(() => {
-    const getRestaurants = async () => {
-      const response = await fetch(
-        `https://redi-final-restaurants.herokuapp.com/restaurants`
-      );
-      const data = await response.json();
-      console.log(data.results);
-      setRestaurants(data.results);
-    };
-    getRestaurants();
-  }, []);
-
   return (
-    <>
-      <ul>
-        {restaurants.map((item) => {
-          return (
-            <li key={item.id}>
-              {item.name}
-              {item.formatted_address}{' '}
-              {item.opening_hours.open_now ? 'open' : 'closed'}{' '}
-              {item.pickup ? 'pickup' : null}{' '}
-              {item.delivery ? 'delivery' : null}
-              <br />
-              <img src={item.photos[0].links[3]} alt="icon" />
-            </li>
-          );
-        })}
-      </ul>
-    </>
+    <div className="App">
+      <NavBar />
+      <div>
+        <SearchBar />
+      </div>
+
+      <div>
+        <ListOfRestaurants />
+      </div>
+      <Footer />
+    </div>
   );
 }
 
