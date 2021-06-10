6ae0246434d7e0c9fff02bc31f8e01633b0de24c
khoolood <khoolood.loolood@gmail.com>
2021-06-10 16:57:49 +0200

    fixing-restaurantbutton- component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import './App.css';
-import { NavBar } from './Components/NavBar';
-import { SearchBar } from './Components/SearchBar';
-import { ListOfRestaurants } from './Components/ListOfRestaurants';
-import { Footer } from './Components/Footer';
-import { FilteringCategories } from './Components/FilteringCategories';
-import { RestaurantsButton } from './Components/RestaurantsButton';
+import "./App.css";
+
+import React, {useState} from 'react'
+import { NavBar } from "./Components/NavBar";
+import { SearchBar } from "./Components/SearchBar";
+import { ListOfRestaurants } from "./Components/ListOfRestaurants";
+import { Footer } from "./Components/Footer";
+import { FilteringCategories } from "./Components/FilteringCategories";
+import { RestaurantsButton } from "./Components/RestaurantsButton";
+
 
 function App() {
+    const [restaurantsOn, setRestaurantsOn] = useState(false);
+  const restaurantsButtonHandler = () => {
+    setRestaurantsOn(!restaurantsOn);
+  };
   return (
     <div className="App">
       <NavBar />
+      
       <div>
         <SearchBar />
       </div>
       <div class="bottombar">
-        <RestaurantsButton />
+        <RestaurantsButton restaurantsOn={restaurantsOn} restaurantsButtonHandler={restaurantsButtonHandler} />
         <FilteringCategories />
       </div>
       <hr />
 
       <div>
-        <ListOfRestaurants />
+        {restaurantsOn && <ListOfRestaurants />}
       </div>
       <Footer />
     </div>
