580a8894f488c146e218bbf60981b88b7c8b63e1
khoolood <khoolood.loolood@gmail.com>
2021-06-16 14:05:43 +0200

    Routing details page fixing 3

--- src/Components/Restaurant.jsx
+++ src/Components/Restaurant.jsx
@@ -3,11 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const Restaurant = ({ item }) => {
   return (
-    <Link
-      to={`/detailspage/${item.id}`}
-      style={{ textDecoration: 'none' }}
-      target="_blank"
-    >
+    <Link to={`/detailspage/${item.id}`} style={{ textDecoration: 'none' }}>
       <li class="restaurant_card">
         <div class="restaurant_image">
           <img class="image" src={item.photos[0].links[0]} alt="icon" />
