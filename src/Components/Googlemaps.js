1de78d17c6feda46dab17c72f91903919723d659
Isabbelle <isabelle.muresan@gmail.com>
2021-06-17 15:29:29 +0200

    fixing map

--- src/Components/Googlemaps.js
+++ src/Components/Googlemaps.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
 function Googlemaps() {
+  const WrappedMap = withScriptjs(withGoogleMap(Googlemaps));
   <GoogleMap
     defaultZoom={10}
     defaultCenter={{ lat: 52.520008, lng: 13.404954 }}
   />;
-  const WrappedMap = withScriptjs(withGoogleMap(Map));
+
   return (
-    <div className="mapcontainer" style={{ width: '50vw', height: '50vh' }}>
+    <div className="mapcontainer" style={{ width: '100vw', height: '50vh' }}>
       <WrappedMap
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: '100%' }} />}
-        containerElement={<div style={{ height: '100%' }} />}
-        mapElement={<div style={{ height: '100%' }} />}
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
+        loadingElement={<div style={{ height: '50%' }} />}
+        containerElement={<div style={{ height: '50%' }} />}
+        mapElement={<div style={{ height: '50%' }} />}
       />
     </div>
   );
