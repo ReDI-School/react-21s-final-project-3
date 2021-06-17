169a99c2031e14af06673a9cff427408e7760bec
khoolood <khoolood.loolood@gmail.com>
2021-06-17 11:24:14 +0200

    About page-fix-1

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const About = () => {
   return (
-    <div>
-      <h1>Made with love by Isabelle Muresan & Khouloud Karoui</h1>
+    <div class="madewith">
+      <h1>Made with love by Isabelle Muresan & Khouloud Karoui &hearts;</h1>
     </div>
   );
 };
